eaaa2cb7f6f887a074bf22e2da8b09ff8cd28a35
Sarah Lawrence <sarahlawrence1152@gmail.com>
2021-03-29 20:00:42 -0400

    Created comments for my commented out code

--- api/groomerFavorites/favRouter.js
+++ api/groomerFavorites/favRouter.js
@@ -20,16 +20,26 @@ router.get('/', async (req, res) => {
     const data = await groomerFavModel.getAll();
     res.status(200).json(data);
   } catch (err) {
-    res.status(500).json({ message: err.message })
+    res.status(500).json({ message: err.message });
   }
 });
 
-
+// See notes on the favModel regarding the errors we encountered
+// The following code is needed if wanting to display more groomer info/data
+// on the pet owners dashboard.
+//router.get('/:id', async (req, res) => {
+//try {
+//const data = await groomerFavModel.getById(req.params.id);
+//res.status(200).json(data);
+//} catch (err) {
+// res.status(500).json({ message: err.message });
+//  }
+//});
 
 //============================//
 //  DELETE groomer by id //
 //============================//
-// The routes work without 'authRequired'. This code will be needed to verify id token. 
+// The routes work without 'authRequired'. This code will be needed to verify id token.
 // We were not able to get 'authRequired' working at this time.
 // router.delete('/:id', authRequired, async (req, res) => {
 router.delete('/:id', async (req, res) => {
@@ -43,6 +53,5 @@ router.delete('/:id', async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 });
-
 
 module.exports = router;
