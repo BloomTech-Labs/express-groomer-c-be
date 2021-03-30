58ab6bf19bfa3f967b21525157ec4d5570dc56d0
Sarah Lawrence <sarahlawrence1152@gmail.com>
2021-03-29 20:09:26 -0400

    editing my comments on the favModel

--- api/groomerFavorites/favModel.js
+++ api/groomerFavorites/favModel.js
@@ -7,7 +7,7 @@ const getAll = async () => {
 // The following code is meant to grab data from the groomer table and display when user id is matched
 // to id. Unfortunately, I ran in to an error. When user_id is compared to id,
 // an error is thrown because user_id is a string and id is an integer. If this code is eventually used, all of the
-// migrations will need to be changed because all of the ids are set as strings.
+// migrations will need to be changed because all of the ids are defined as strings.
 //
 // The following code is needed if you want to display more data on the pet owners dashboard.
 //const getById = async (id) => {
